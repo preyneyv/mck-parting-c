53a5962311d4d80547645fe6d701ec4cdcb37dd6
Pranav Nutalapati <pranavnutalapati@gmail.com>
2025-07-03 02:38:59 -0400

    feat: got screen working, added timing instruments

--- src/config.h
+++ src/config.h
@@ -1,13 +1,12 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
+#include <stdint.h>
+
 //// SYSTEM CONFIGURATION ////
 /** System clock in KHz */
 #define SYS_CLOCK_KHZ 176000
-
-
-
-
+#define TARGET_FPS 10000
 
 //// AUDIO CONFIGURATION ////
 /** GPIO used for audio output */
@@ -20,4 +19,5 @@
 #error "AUDIO_DAC_SAMPLE_RATE is not a divisor of SYS_CLOCK_KHZ"
 #endif
 
+static const uint32_t TARGET_FRAME_INTERVAL_US = 1000000 / TARGET_FPS;
 #endif // CONFIG_H
