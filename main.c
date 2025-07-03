8aa7b76a71370c271ed622eba1fe8decaaabc601
Pranav Nutalapati <pranavnutalapati@gmail.com>
2025-07-02 22:21:46 -0400

    feat: moved audio onto core 1

--- main.c
+++ main.c
@@ -12,18 +12,26 @@
 #include "config.h"
 #include "audio.h"
 
+void core1_entry()
+{
+    audio_init();
+    while (1)
+    {
+        synth_fill_buffers();
+        __wfi();
+    }
+}
+
 int main()
 {
     stdio_init_all();
     set_sys_clock_khz(SYS_CLOCK_KHZ, true);
 
-    audio_init();
+    multicore_reset_core1();
+    multicore_launch_core1(core1_entry);
 
     while (1)
     {
-        synth_fill_buffers();
-        // sleep_us(100);
-        // // Sleep until next buffer fill is needed
-        // __wfi();
+        __wfi();
     }
 }
