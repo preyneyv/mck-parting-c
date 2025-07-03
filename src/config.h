970d2245b4969643cb30ad5dca85de259ad4db58
Pranav Nutalapati <pranavnutalapati@gmail.com>
2025-07-02 22:47:46 -0400

    feat: vendor u8g2

--- src/config.h
+++ src/config.h
@@ -1,19 +1,19 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-/**
- * System clock in KHz
- */
+//// SYSTEM CONFIGURATION ////
+/** System clock in KHz */
 #define SYS_CLOCK_KHZ 176000
 
-/**
- * GPIO used for audio output
- */
+
+
+
+
+//// AUDIO CONFIGURATION ////
+/** GPIO used for audio output */
 #define AUDIO_DAC_PIN 26
 
-/**
- * DAC sample rate in Hz
- */
+/** DAC sample rate in Hz */
 #define AUDIO_DAC_SAMPLE_RATE 22000
 
 #if SYS_CLOCK_KHZ % AUDIO_DAC_SAMPLE_RATE != 0
